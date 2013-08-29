92da45b4bf3d1a99e2d3c4fda13c8bfc86893f29
chsieh6 <charles.hsieh6@gmail.com>
2013-08-29 12:22:58 -0700

    added home page & about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Travelfig::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/contact"
+
+  root to: 'static_pages#home'
+  get '/contact',   to: 'static_pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
