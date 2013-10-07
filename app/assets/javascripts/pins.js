e2d0c0da0f5391d3300704e4abc333dcfbd47bfc
chsieh6 <charles.hsieh6@gmail.com>
2013-10-07 15:16:17 -0700

    fixed responsive design, image reload issues

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,22 +1,60 @@
 $(document).ready(function(){
+
   var $container = $('#isotope-container');
-  $container.imagesLoaded(function(){
-    $container.isotope({
-      itemSelector: '.box'
+  $('#myTab a:first').tab('show');
+  loadPin();
+
+  function pageLoad() {
+    $('.category_button').hide();
+    $('.add_button').hide();
+      
+    $('.box').hover(function(){
+      $($(this).find('.category_button')).show();
+    }, function (){
+      $($(this).find('.category_button')).hide();
     });
-    $container.infinitescroll({
-      navSelector  : '.pagination',    // selector for the paged navigation 
-      nextSelector : '.next_page a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.box',     // selector for all items you'll retrieve
-      loading: {
-          finishedMsg: 'No more images to load.',
-          img: 'http://i.imgur.com/qkKy8.gif'
-        }
-      },
-      // call Isotope as a callback
-      function( newElements ) {
-        $container.isotope( 'appended', $( newElements ) ); 
+     //If it is not the current user
+    $('.box').hover(function(){
+      $($(this).find('.add_button')).show();
+    }, function (){
+      $($(this).find('.add_button')).hide();
+    });
+  }
+
+  // Load all the pins once the page is ready
+  function loadPin() {
+    $container.imagesLoaded(function(){
+      $container.isotope({
+        itemSelector: '.box'
+      });
+    });
+    // Load all the buttons that goes onto the pins
+    pageLoad();
+  }
+
+  // Infinite Scroll setup with a isotope callback
+  $container.infinitescroll({
+    navSelector  : '.pagination',    // selector for the paged navigation 
+    nextSelector : '.next_page a',  // selector for the NEXT link (to page 2)
+    itemSelector : '.box',     // selector for all items you'll retrieve
+    loading: {
+        finishedMsg: 'No more images to load.',
+        img: 'http://i.imgur.com/qkKy8.gif'
       }
-    );  
+    },
+    // call Isotope as a callback
+    function( newElements ) {
+      var $newElems = $(newElements);
+      $newElems.imagesLoaded(function(){
+        $container.isotope('appended', $newElems );
+      });
+      pageLoad();
+    }
+  );
+
+  $('.pin_link').click(function(){
+    var nextPage = $(this).attr('href');
+    location.replace(nextPage);
+    location.reload;
   });
 })
